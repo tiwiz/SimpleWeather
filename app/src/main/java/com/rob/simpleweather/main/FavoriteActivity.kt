c0b352d228d75bd80db6c7c64276ee275d978802
tiwiz <roberto.orgiu@gmail.com>
2020-09-18 17:37:22 +0200

    Create third part of UI

---
+++ app/src/main/java/com/rob/simpleweather/main/FavoriteActivity.kt
@@ -0,0 +1,15 @@
+package com.rob.simpleweather.main
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.rob.simpleweather.R
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
